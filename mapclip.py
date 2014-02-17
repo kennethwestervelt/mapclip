3e3539956c435d5e94c3d78496d03fc32e1925b6
kennethwestervelt <kenneth.westervelt@gmail.com>
2014-02-16 23:38:44 -0700

    Update mapclip.py
    First mostly-working script. Tested with one image.

--- mapclip.py
+++ mapclip.py
@@ -1,31 +1,55 @@
 from PIL import Image
-
-#Argument for input file goes here later.
-
-print "This script will take the image ... and break it up into many smaller images of equal size."
-
+import sys     
+#Error check for input file goes here later.
+     
+im = Image.open(sys.argv[1])
+im.copy()
+im.size
+width = im.size[0] 
+height = im.size[1] 
+print "***********************************\n"     
+print "MapClip will take the image \"" + str(sys.argv[1]) + "\" and break it up into many smaller images of equal size."
+print "Your image is " + str(width) + "px wide and " + str(height) +"px tall."
+     
 newImage = 0
 x=0
 y=0
-xOffset = input('Please input the length of each image. \n')
+     
+# Alpha offset procedure. A database of popular/useful image sizes is on the wishlist.
+     
+xOffset = input('Please input the width of each image. \n')
 yOffset = input('Please input the height of each image. \n')
-length = 100 #Sample number here. Add length.img later.
-height = 100 #Sample number here. Add width.img later.
 
+# Verifies all images will be of equal size.
+
+if width % xOffset !=0
+  print "This would create images of unequal size. Try a different width."
+  exit()
+  
+if height % yOffset !=0
+  print "This would create images of unequal size. Try a different height."
+  exit()
+
+# A useful error check to prevent 0px images.     
 if xOffset >= 1 and yOffset >= 1:
-#  xModulus = length % xOffset
-#  yModulus = height % yOffset
-#  if xModulus != 0 or yModulus !=0
-#    print "This program cannot divide the image into unequal pieces."
-#    sys.exit() 
+     
   while y < height:
-    while x < length:
+    while x < width:
       newImage +=1
-      #more stuff here later
+      img_crop = im.crop((x,y,x+xOffset,y+yOffset))
+      img_crop.save (str(newImage), "PNG")   
       x = x + xOffset
-      
+           
     x = 0
     y = y + yOffset
   print "There will be a total of " + str(newImage) +" images."
+  print "Thanks for using MapClip!"
+  im.close(sys.argv[1])
+  exit()
+     
+     
 else:
-  print "nope"
+  print "Cannot create 0x0px files, sorry. That would destroy your hard drive, I think."
+  im.close(sys.argv[1])
+  exit()
+
